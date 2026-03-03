4b95478c272e6b6179f6a122a09a318732d35a6c
sarvesh Tiwari <sarveshscientist1590@gmail.com>
2026-03-03 15:45:37 +0530

    Day 1: Initial setup with architecture diagram and folder structure

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "subscription_id" {
+  description = "The subscription ID for the Azure resources."
+  type        = string
+}
+variable "location" {
+  description = "The Azure region where resources will be created."
+  type        = string
+  default     = "East US"
+}
+variable "environment" {
+  description = "The environment for the resources (e.g., dev, prod)."
+  type        = string
+  default     = "production"
+}
